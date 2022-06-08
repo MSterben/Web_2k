472daff0cc3fe8714c09b1a57442d4681e95d756
Stasian-Y <give2052@gmail.com>
2022-06-08 08:31:56 +0300

    half of the lab

--- 10-th/js/ajax.js
+++ 10-th/js/ajax.js
@@ -1,4 +1,10 @@
+
 const requestURL = 'https://usersdogs.dmytrominochkin.cloud/dogs';
+
+
+
+
+
 
 let open_modal = document.querySelectorAll('.open_modal');
 let close_modal = document.getElementById('close_modal');
@@ -7,36 +13,20 @@ let body = document.getElementsByTagName('body')[0];
 
 for (let i = 0; i < open_modal.length; i++) {
     open_modal[i].onclick = function() { 
-
-    //xhr
-
-    const baseSrc = 'https://usersdogs.dmytrominochkin.cloud';
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', requestURL);
-    xhr.responseType = 'json';
-    xhr.onload = () =>{
-    if(xhr.status >= 400){
-         console.error(xhr.response)
-     }
-    else{
-    // chenging of modal window
-    document.getElementById("link").src = baseSrc + xhr.response[i].dogImage;
-    document.getElementById("name").innerHTML = xhr.response[i].title;
-    document.getElementById("sex").innerHTML = xhr.response[i].sex;
-    document.getElementById("age").innerHTML = xhr.response[i].age;
-    document.getElementById("description").innerHTML = xhr.response[i].description;
-    
-    //manipulation with elements
-
+      const baseSrc = 'https://usersdogs.dmytrominochkin.cloud';
+      // ajax
+      $.ajax(requestURL,{
+        success: function(data){
+    document.getElementById("link").src = baseSrc + data[i].dogImage;
+    document.getElementById("name").innerHTML = data[i].title;
+    document.getElementById("sex").innerHTML = data[i].sex;
+    document.getElementById("age").innerHTML = data[i].age;
+    document.getElementById("description").innerHTML = data[i].description;
     modal.classList.add('modal_vis'); 
-    body.classList.add('body_block'); 
-
-    }
-}
-    
-xhr.send();
-
-
+    body.classList.add('body_block');  
+        }
+      });
+      
 //end of xhr
         
     };
@@ -45,6 +35,6 @@ xhr.send();
 // close modal
 
 close_modal.onclick = function() {  
-    modal.classList.remove('modal_vis');
-    body.classList.remove('body_block'); 
+  modal.classList.remove('modal_vis');
+  body.classList.remove('body_block'); 
 };
