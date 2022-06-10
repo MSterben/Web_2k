eba62af3c51e8e1697594a519d912325b1f650ce
Stasian-Y <give2052@gmail.com>
2022-06-10 19:59:10 +0300

    new version lab  9

--- 9lab/js/main.js
+++ 9lab/js/main.js
@@ -1,17 +1,44 @@
 const requestURL = 'https://usersdogs.dmytrominochkin.cloud/dogs';
 const baseSrc = 'https://usersdogs.dmytrominochkin.cloud';
 const xhr = new XMLHttpRequest();
-let open_modal = document.querySelectorAll('.open_modal');
 let close_modal = document.getElementById('close_modal');
-let modal = document.getElementById('modal');
 let body = document.getElementsByTagName('body')[0];
 
 
-for (let i = 0; i < open_modal.length; i++) {
-    open_modal[i].onclick = function() { 
 
-    //xhr
+let x = 0;
 
+xhr.open('GET', requestURL);
+    xhr.responseType = 'json';
+    xhr.onload = () =>{
+    if(xhr.status >= 400){
+         console.error(xhr.response)
+     }
+    else{
+        let kost = xhr.response;
+        kost.forEach(element => {
+            main.insertAdjacentHTML("beforebegin", 
+            `<div class="grid open_modal" onclick="display(this)"  value="${x}">
+            <img src="${baseSrc}${element.dogImage}" alt="dog">
+            <div class="textAling">
+            <h2>${element.title}</h2>
+            <p>${element.sex}</p>
+            </div>
+            </div>`);
+            x++;
+
+            
+        });
+    }
+}
+    
+xhr.send();
+
+
+
+function display(elm) {
+   
+    var x = elm.getAttribute('value');
     xhr.open('GET', requestURL);
     xhr.responseType = 'json';
     xhr.onload = () =>{
@@ -20,11 +47,11 @@ for (let i = 0; i < open_modal.length; i++) {
      }
     else{
     // chenging of modal window
-    document.getElementById("link").src = baseSrc + xhr.response[i].dogImage;
-    document.getElementById("name").innerHTML = xhr.response[i].title;
-    document.getElementById("sex").innerHTML = xhr.response[i].sex;
-    document.getElementById("age").innerHTML = xhr.response[i].age;
-    document.getElementById("description").innerHTML = xhr.response[i].description;
+    document.getElementById("link").src = baseSrc + xhr.response[x].dogImage;
+    document.getElementById("name").innerHTML = xhr.response[x].title;
+    document.getElementById("sex").innerHTML = xhr.response[x].sex;
+    document.getElementById("age").innerHTML = xhr.response[x].age;
+    document.getElementById("description").innerHTML = xhr.response[x].description;
     
     //manipulation with elements
 
@@ -32,17 +59,14 @@ for (let i = 0; i < open_modal.length; i++) {
     body.classList.add('body_block'); 
 
     }
+
 }
     
 xhr.send();
+  }
 
-
-//end of xhr
-        
-    };
-}
-
-// close modal
+  
+// // close modal
 
 close_modal.onclick = function() {  
     modal.classList.remove('modal_vis');
